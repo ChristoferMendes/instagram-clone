4df22b065d051421f8e3cbf2bb0265017d890cce
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-15 13:42:43 -0300

    add the menu and profile image at the right side of header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import Header from '@/components/Header'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
   title: 'Create Next App',
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
     <body className={inter.className}>
-    <Header />
+    <Header/>
     {children}
     </body>
     </html>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import { MagnifyingGlassIcon, PlusCircleIcon } from '@heroicons/react/24/outline'
+import { HomeIcon } from '@heroicons/react/24/solid'
+import { faker } from '@faker-js/faker'
 
 export default function Header() {
   return (
-    <div className='flex items-center justify-between max-w-6xl'>
+    <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
       <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
         <Image
           alt='instagram'
@@ -28,10 +30,16 @@ export default function Header() {
         <div className='absolute top-2 left-2'>
           <MagnifyingGlassIcon className='h-5 text-gray-500' />
         </div>
-        <input type="text" name="" id="" title='text' placeholder='Search' className='bg-gray-50 pl-10  border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'/>
+        <input type="text" name="" id="" title='text' placeholder='Search' className='bg-gray-50 pl-10  border-gray-500 text-sm focus:ring-black focus:border-black rounded-md' />
       </div>
 
-      <h1>Right side</h1>
+      <div className='flex space-x-4 items-center'>
+        <HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out' />
+        <PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out' />
+        <Image src={faker.image.avatar()} alt="username" width={'42'} height={'42'} className='rounded-full cursor-pointer'/>
+      </div>
     </div>
   )
+
+
 }
