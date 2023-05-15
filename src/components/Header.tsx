b24a164dbd74b56aa6574effc9d1ab64a7baec28
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-15 13:27:26 -0300

    Add @heroicons/react and @tailwindcss/forms dependencies and include search input field with magnifying glass icon in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,37 @@
 import Image from 'next/image'
 import React from 'react'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 
 export default function Header() {
   return (
     <div className='flex items-center justify-between max-w-6xl'>
-      <div>
-        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-          <Image
-            alt='instagram'
-            src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
-            fill
-            className="object-contain"
-          // onClick={() => router.push("/")}
-          />
-        </div>
-        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-          <Image
-            alt='instagram'
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
-            fill
-            className="object-contain"
-          // onClick={() => router.push("/")}
-          />
-        </div>
+      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+        <Image
+          alt='instagram'
+          src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
+          fill
+          className="object-contain"
+        // onClick={() => router.push("/")}
+        />
       </div>
-        <h1>Right sides</h1>
+      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+        <Image
+          alt='instagram'
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
+          fill
+          className="object-contain"
+        // onClick={() => router.push("/")}
+        />
+      </div>
+
+      <div className='relative '>
+        <div className='absolute top-2 left-2'>
+          <MagnifyingGlassIcon className='h-5 text-gray-500' />
+        </div>
+        <input type="text" name="" id="" title='text' placeholder='Search' className='bg-gray-50 pl-10  border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'/>
+      </div>
+
+      <h1>Right side</h1>
     </div>
   )
 }
