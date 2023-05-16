ad044bdb5950e863ae6b94b9b35be6ac7290f386
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-16 15:10:41 -0300

    style the story section and install tailwind-scrollbar

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -15,19 +15,23 @@ export default function Stories() {
   const [storyUsers, setStoryUsers] = useState<User[]>([])
 
   useEffect(() => {
-    const randomNumber = Math.ceil(Math.random() * 70)
 
-    const storyUsers = minifaker.array(20, (i) => ({
-      username: minifaker.username({ locale: 'en' }),
-      img: `https://i.pravatar.cc/150?img=${randomNumber},`,
-      id: i
-    }));
+    const storyUsers = minifaker.array(20, (i) => {
+      const randomNumber = Math.ceil(Math.random() * 70)
+
+      return {
+        username: minifaker.username({ locale: 'en' }),
+        img: `https://i.pravatar.cc/150?img=${randomNumber},`,
+        id: i
+      }
+    });
 
     setStoryUsers(storyUsers);
   }, [])
 
   return (
-    <div>
+    <div
+      className={'flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none'}>
       {storyUsers.map(user => (
         <Story key={user.id} img={user.img} username={user.username} />
       ))}
